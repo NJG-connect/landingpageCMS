c13d06a0ec94234bf853d15142489a7a9281484a
jeremynoh <jeremynohile@gmail.com>
2021-07-05 17:05:26 +0200

    try fix chekout version for install node  on github action

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import styles from "./AdminPanel.module.css";
 
-interface Props {}
-
-function AdminPanel({}: Props) {
+function AdminPanel() {
   async function onClick() {
     console.log("try something");
 
@@ -12,9 +10,12 @@ function AdminPanel({}: Props) {
     const url = `https://api.github.com/repos/${owner}/${name}/dispatches`;
     let response = { succes: false, data: undefined };
     const body = {
-      name: "voici le nom",
-      title: "ceci est un titre",
-      description: "et ceci est une description",
+      event_type: "handle-submission",
+      client_payload: {
+        name: "voici le nom",
+        title: "ceci est un titre",
+        description: "et ceci est une description",
+      },
     };
     try {
       const responseFetch = await fetch(url, {
@@ -25,7 +26,7 @@ function AdminPanel({}: Props) {
         },
         body: body ? JSON.stringify(body) : null,
       });
-      const responseJson = await responseFetch.json();
+      const responseJson: any = "success";
       response = { succes: responseFetch.ok, data: responseJson };
     } catch (error) {
       response = { succes: false, data: error };
